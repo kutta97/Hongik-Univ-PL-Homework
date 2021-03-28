b0b82ea63c4bb56306c3b78c4884a67ab64a449a
kutta97 <yhjin04170@gmail.com>
2021-03-28 17:18:16 +0900

    complete hw1_1: binary search

---
+++ hw1c/hw1_1.py
@@ -0,0 +1,18 @@
+def binary_search(list, target, low, high) -> int:
+	if low >= high:
+		return -1
+	mid = (low + high) // 2
+	if list[mid] > target:
+		return binary_search(list, target, low, mid)
+	if list[mid] == target:
+		return mid
+	if list[mid] < target:
+		return binary_search(list, target, mid + 1, high)
+	else:
+		return -1
+
+list = list(map(int, input().split()))
+target = int(input())
+output = binary_search(list, target, 0, len(list) - 1)
+
+print("None" if output < 0 else output + 1)
