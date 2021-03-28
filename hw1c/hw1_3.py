d48b10f0a754686c490346441b9c3986f892c878
kutta97 <yhjin04170@gmail.com>
2021-03-28 17:40:34 +0900

    complete hw1_3: merge sort

---
+++ hw1c/hw1_3.py
@@ -0,0 +1,22 @@
+def merge_sort(list):
+	if len(list) < 2:
+		return list
+	mid = len(list) // 2
+	low = merge_sort(list[:mid])
+	high = merge_sort(list[mid:])
+
+	merged = []
+	l = h = 0
+	while l < len(low) and h < len(high):
+		if low[l] < high[h]:
+			merged.append(low[l])
+			l += 1
+		else:
+			merged.append(high[h])
+			h += 1
+	merged += low[l:]
+	merged += high[h:]
+	return merged
+
+list = list(map(int, input().split()))
+print(merge_sort(list))
