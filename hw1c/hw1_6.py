d497e91ec628b29e07e51a84c9d021353d5a5e11
kutta97 <yhjin04170@gmail.com>
2021-03-31 01:20:04 +0900

    complete hw1_6: data encryption alaysis

---
+++ hw1c/hw1_6.py
@@ -0,0 +1,25 @@
+PATTERN = "(100~1~|01)~"
+
+def data_encryption_analyis(data):
+	i = 0
+	while i < len(data):
+		if len(data[i:]) >= len("100") and data[i:i+3] == "100":
+			i += len("100")
+			while i < len(data) and data[i] == '0': i+= 1
+			if i == len(data): print("PASS"); return
+			if data[i] == '1': 
+				while i < len(data) and data[i] == '1': i+= 1
+			else: print("PASS"); return
+		if len(data[i:]) >= len("01") and data[i:i+2] == "01":
+			i += len("01"); continue
+		print("PASS"); return
+	print("DANGER")
+
+count = int(input())
+
+data_set = []
+for i in range(count):
+	data_set.append(input())
+
+for data in data_set:
+	data_encryption_analyis(data)
