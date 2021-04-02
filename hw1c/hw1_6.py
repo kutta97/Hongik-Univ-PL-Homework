d760d2cf5863d6db7ac834d7303240cf11b4f7ac
kutta97 <yhjin04170@gmail.com>
2021-04-02 22:06:29 +0900

    change hw1_6 to use regex

--- hw1c/hw1_6.py
+++ hw1c/hw1_6.py
@@ -1,21 +1,4 @@
-PATTERN = "(100~1~|01)~"
-
-def data_encryption_analyis(data):
-	i = 0
-	while i < len(data):
-		if len(data[i:]) >= len("100") and data[i:i+3] == "100":
-			i += len("100")
-			while i < len(data) and data[i] == '0': i+= 1
-			if i == len(data): print("PASS"); return
-			if data[i] == '1': 
-				while i < len(data) and data[i] == '1': i+= 1
-			else: print("PASS"); return
-			if i < len(data) - 1 and data[i-2:i+2] == "1100": i -= 1
-			continue
-		if len(data[i:]) >= len("01") and data[i:i+2] == "01":
-			i += len("01"); continue
-		print("PASS"); return
-	print("DANGER")
+import re
 
 count = int(input())
 
@@ -24,4 +7,5 @@ for i in range(count):
 	data_set.append(input())
 
 for data in data_set:
-	data_encryption_analyis(data)
+	if re.fullmatch('(100+1+|01)+', data): print("DANGER")
+	else: print("PASS")
