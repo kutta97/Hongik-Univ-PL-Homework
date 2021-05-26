737fbd1963f68ee0edba2b2d485acec0afeb5f71
kutta97 <yhjin04170@gmail.com>
2021-05-26 23:28:56 +0900

    add hanoi

---
+++ hw5c/hanoi.pl
@@ -0,0 +1,15 @@
+hanoi(N):-
+	move(N,1,2,3).	
+
+move(N,Start,End,Via):-
+	N =:= 1 ->
+	print(N,Start,End);
+	N1 is N - 1,
+	move(N1,Start,Via,End),
+	print(N,Start,End),
+	move(N1,Via,End,Start).
+
+print(N,Start,End):-
+	write(N), write('->['),
+	write(Start), write(','),
+	write(End), write(']'), nl.
